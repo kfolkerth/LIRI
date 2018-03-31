32e444028654f9e78c4b0a0dce430ab92ffc3bcd
Kyle <kyle.folkerth@gmail.com>
2018-03-31 12:51:57 -0700

    updates liri.js with package requires

--- liri.js
+++ liri.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var Twitter = require('twitter');
+var Spotfy = require('spotify');
+var Request = require('request');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
