ab7d1781cc4121c1e9ef4b43f7139a9e3d1df142
Kyle <kyle.folkerth@gmail.com>
2018-03-31 12:48:59 -0700

    installs twitter, spotify, request, and dotenv packages

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var Twitter = require('twitter');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
